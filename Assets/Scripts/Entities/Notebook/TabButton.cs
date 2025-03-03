dcbc6d8e1fe2f1972a2254d1cf461beb7ad6c118
Zekai "Zk" Zhang <zz2919@columbia.edu>
2025-03-03 14:34:45 -0800

    wip: notebook tab interface

---
+++ Assets/Scripts/Entities/Notebook/TabButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+    public Image background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

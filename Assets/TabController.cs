8039f552d03ccefb74cf00a6b100e68737192447
Zekai "Zk" Zhang <zz2919@columbia.edu>
2025-03-04 16:36:41 -0800

    feat: functional tabController!

--- Assets/TabController.cs
+++ Assets/TabController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image[] tabImages; 
+    public GameObject[] pages;
+    
     void Start()
     {
-        
+        ActivateTab(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateTab(int tabIndex)
     {
-        
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+            tabImages[i].color = Color.gray;
+        }
+        pages[tabIndex].SetActive(true);
+        tabImages[tabIndex].color = Color.white;
     }
 }
